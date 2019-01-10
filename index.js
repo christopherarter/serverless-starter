95ebafd4f9d962da3b5e77d5989afdb9be08521a
Chris Arter <carter@AdministorsMBP2.fios-router.home>
2019-01-10 12:35:08 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+// index.js
+
+// const serverless = require('serverless-http');
+// const express = require('express');
+import serverless from 'serverless-http';
+import express from 'express';
+const app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+module.exports.handler = serverless(app);
